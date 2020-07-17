127a32f109d5e3173246c4cf5ca6a722b8c1f8c3
Renaud <contact.dogeoffire@gmail.com>
2020-07-18 01:07:09 +0200

    Addition of comments.

--- functions/init.py
+++ functions/init.py
@@ -1,3 +1,6 @@
 # This code is not necessary, it is only there to inform about the program when it starts.
+# ---
+# Definition of the "init" function.
 def init():
     print('\n\n        PyPass - version 0.1\nhttps://github.com/TheClio69/PyPass\n')
+# --- End of file --- #
