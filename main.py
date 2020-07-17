faeacbc24875f46d0b54f3c18d454380ae4d069e
Renaud <contact.dogeoffire@gmail.com>
2020-07-18 00:46:22 +0200

    Addition of comments.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# Imports of required files.
 import site
+# Defining the project path for imports.
 site.addsitedir('/')
+# Continued import of required files.
 from functions.init import *
 from functions.config import *
 
+# Viewing program information.
 init()
+# Start of the configuration required to generate one or more passwords.
 config()
