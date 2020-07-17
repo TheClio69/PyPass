3d62560d299f3c909287412ed7e364eafd9c3a83
Renaud <contact.dogeoffire@gmail.com>
2020-07-18 00:48:17 +0200

    Creation of the initialization file.

---
+++ functions/init.py
@@ -0,0 +1,3 @@
+# This code is not necessary, it is only there to inform about the program when it starts.
+def init():
+    print('\n\n        PyPass - version 0.1\nhttps://github.com/TheClio69/PyPass\n')
